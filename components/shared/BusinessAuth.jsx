3c6fd6bcc8955771ebb3f423a245388696dc8f95
Thomas Price <thomasjprice2@gmail.com>
2024-07-26 11:28:57 +0100

    sign up

--- components/shared/BusinessAuth.jsx
+++ components/shared/BusinessAuth.jsx
@@ -4,27 +4,40 @@ import { createClient } from "@/utils/supabase/client"
 import { Input } from "../ui/input"
 import { Button } from "../ui/button"
 import { useState } from "react"
+import toast from "react-hot-toast"
 
 const supabase = createClient()
 
 async function signInWithEmail() {
-  const { data, error } = await supabase.auth.signInWithOtp({
-    email: 'thomasjprice2@gmail.com',
-    options: {
-      // set this to false if you do not want the user to be automatically signed up
-      shouldCreateUser: false,
-      emailRedirectTo: 'https://example.com/welcome',
-    },
-  })
+  // sign in
 }
 
 const BusinessAuth = () => {
   const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  async function signUpWithEmail() {
+    const data = {
+      email: email,
+      password: password
+    }
+  
+    const { error } = await supabase.auth.signUp(data)
+  
+    if(error) {
+      toast.error(`Error signing up: ${error}`)
+    } else {
+      toast.success('Successfully signed up!')
+    }
+  }
 
   return (
-    <div className="flex gap-2 mt-4">
+    <div className="mt-4 flex flex-col gap-2 items-start">
       <Input type='email' placeholder='Email' className="max-w-[300px]" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <Button onClick={signInWithEmail}>Submit</Button>
+
+      <Input type='password' placeholder='Password' className="max-w-[200px]" value={password} onChange={(e) => setPassword(e.target.value)} />
+
+      <Button onClick={signUpWithEmail}>Sign Up</Button>
     </div>
   )
 }
