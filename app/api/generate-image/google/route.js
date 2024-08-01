c041e914bde313d698feb98729af2f1992ae8db8
Thomas Price <thomasjprice2@gmail.com>
2024-08-01 17:35:34 +0100

    working images

--- app/api/generate-image/google/route.js
+++ app/api/generate-image/google/route.js
@@ -1,13 +1,10 @@
-import { icons } from "@/utils/constants";
 import { ImageResponse } from "@vercel/og";
-
-import Bean from '@/assets/icons/bean.svg'
+import Bean from '@/assets/icons/bean.svg';
 
 export async function GET() {
   const BGCOLOUR = '#5C3E32';
-  const QTY = 9 + 1; // Buy 4 get 1 free, total of 5 icons
-  const POINTS = 2; // Number of points achieved
-  const ICON = icons.coffeeCup;
+  const QTY = 4 + 1; // Buy 4 get 1 free, total of 10 icons
+  const POINTS = 0; // Number of points achieved
 
   const renderIcons = (start, end) => {
     return Array.from({ length: end - start }).map((_, index) => {
@@ -21,8 +18,8 @@ export async function GET() {
       const borderColor = isAchieved || (isLast && lastAchieved) ? (isLast ? '#77dd77' : '#ff9900') : '#cccccc';
       const iconColor = isAchieved || (isLast && lastAchieved) ? (isLast ? '#77dd77' : '#ff9900') : '#cccccc';
 
-      const width = (904 - (16 * (QTY - 1))) / QTY
-      const largeWidth = (904 - (16 * ((QTY / 2) - 1))) / (QTY / 2)
+      const width = (904 - (16 * (QTY - 1))) / QTY;
+      const height = (256 / 2)
 
       return (
         <div
@@ -30,21 +27,12 @@ export async function GET() {
           tw={`flex justify-center rounded-full border ${bgColor} ${QTY <= 5 ? 'my-auto p-6' : 'p-3'}`}
           style={{
             borderColor: borderColor,
-            width: `${largeWidth}px`,
-            height: `${largeWidth}px`,
+            width: QTY <= 5 ? `${width}px` : `${height}px`,
+            height: QTY <= 5 ? `${width}px` : `${height}px`,
             color: iconColor
           }}
         >
-          {/* <Coffee
-            // src={ICON}
-            alt="icon"
-            style={{
-              width: '100%',
-              height: '100%',
-              padding: QTY <= 6 ? '0' : '8px',
-            }}
-          /> */}
-          <Bean tw={`w-full h-full flex ${QTY <= 5 ? '' : 'p-2'}`} />
+          <Bean tw={`w-full h-full ${QTY <= 5 ? '' : 'p-2'}`} fill={iconColor} />
         </div>
       );
     });
@@ -56,18 +44,18 @@ export async function GET() {
     (
       <div tw="w-full h-full flex flex-col justify-between px-16 py-8" style={{ backgroundColor: BGCOLOUR }}>
         {QTY <= 5 ? (
-          <div tw="flex justify-center gap-4 h-full" style={{ gap: '16px' }}>
+          <div tw="flex justify-center gap-4 my-auto" style={{ gap: '16px' }}>
             {renderIcons(0, QTY)}
           </div>
         ) : (
-          <>
-            <div tw="flex justify-center gap-4 h-full">
+          <div tw="w-full h-full flex flex-col justify-between" style={{ gap: '16px'}}>
+            <div tw="flex justify-center gap-4" style={{ gap: '16px' }}>
               {renderIcons(0, halfQty)}
             </div>
-            <div tw="flex justify-center gap-4 h-full">
+            <div tw="flex justify-center gap-4" style={{ gap: '16px' }}>
               {renderIcons(halfQty, QTY)}
             </div>
-          </>
+          </div>
         )}
       </div>
     ),
@@ -76,7 +64,6 @@ export async function GET() {
       height: 336,
       status: 200,
       statusText: "Ok",
-      emoji: 'openmoji'
     }
   );
 }
