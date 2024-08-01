c818a17848423d87f8dc7cf91abfa752e28885c0
Thomas Price <thomasjprice2@gmail.com>
2024-08-01 18:47:03 +0100

    creds stored for prod

--- utils/googlePasses.js
+++ utils/googlePasses.js
@@ -4,13 +4,26 @@ import { redirect } from 'next/navigation';
 
 const { GoogleAuth } = require('google-auth-library');
 const jwt = require('jsonwebtoken');
-const credentials = require('@/creds.json');
 
 const issuerId = '3388000000022364849';
 const baseUrl = 'https://walletobjects.googleapis.com/walletobjects/v1';
 
+const googleCredentials = {
+  type: process.env.GOOGLE_TYPE,
+  project_id: process.env.GOOGLE_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_PRIVATE_KEY,
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_CLIENT_ID,
+  auth_uri: process.env.GOOGLE_AUTH_URI,
+  token_uri: process.env.GOOGLE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN,
+};
+
 const httpClient = new GoogleAuth({
-  credentials: credentials,
+  credentials: googleCredentials,
   scopes: 'https://www.googleapis.com/auth/wallet_object.issuer'
 });
 
